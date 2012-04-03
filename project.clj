cc07e9e1e4791f27447c30d65da84a60502e4698
ryan spangler <ryan.spangler@gmail.com>
2012-04-03 13:49:44 -0700

    breaking out api from the rest of caribou

---
+++ project.clj
@@ -0,0 +1,17 @@
+(defproject caribou-api "0.1.0-SNAPSHOT"
+  :description "The api ring handler for caribou-core"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [antler/caribou "0.3.4-SNAPSHOT"]
+                 [compojure "0.6.4"]
+                 [hiccup "0.3.6"]
+                 [cheshire "2.0.2"]
+                 [org.clojars.ninjudd/data.xml "0.0.1-SNAPSHOT"]
+                 [clojure-csv/clojure-csv "1.3.2"]
+                 [antler/sandbar "0.4.0-SNAPSHOT"]
+                 [ring/ring-jetty-adapter "0.3.10"]]
+  :dev-dependencies [[lein-ring "0.4.5"]
+                     [swank-clojure "1.4.0-SNAPSHOT"]]
+  :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"]
+  :ring {:handler caribou-api.core/app
+         :servlet-name "caribou"
+         :init caribou-api.core/init})
---
+++ test/caribou_api/test/core.clj
@@ -0,0 +1,6 @@
+(ns caribou-api.test.core
+  (:use [caribou-api.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
