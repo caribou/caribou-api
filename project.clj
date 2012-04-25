d820bd524f9cc966d11562f8b44c68f4adda64b3
Ryan Roemmich <ryan@weareinstrument.com>
2012-04-25 16:03:21 -0700

    bump ver

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject antler/caribou-api "0.3.4"
+(defproject antler/caribou-api "0.3.5"
   :description "The api ring handler for caribou"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [antler/caribou-core "0.5.0"]
