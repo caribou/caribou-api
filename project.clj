4405eeb9924c8b9ce29a2774a5cc6f8e82a6ece9
Ryan Roemmich <ryan@weareinstrument.com>
2012-04-25 15:36:57 -0700

    bump dep vers

--- project.clj
+++ project.clj
@@ -1,22 +1,20 @@
-(defproject antler/caribou-api "0.3.1"
+(defproject antler/caribou-api "0.3.4"
   :description "The api ring handler for caribou"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [antler/caribou-core "0.5.0"]
-                 [compojure "1.0.1"]
-                 [ring/ring-core "1.0.2"
+                 [compojure "1.0.2"]
+                 [ring/ring-core "1.1.0"
                   :exclusions [org.clojure/clojure
                                clj-stacktrace]]
                  [hiccup "0.3.6"]
                  [org.clojars.doo/cheshire "2.2.3"]
                  ;; [cheshire "3.1.0"]
-                 [ring/ring-jetty-adapter "1.1.0"]
                  [org.clojure/data.xml "0.0.3"]
                  [clojure-csv/clojure-csv "1.3.2"]
                  [org.clojars.cjschroed/sandbar "0.4.0"]
                  [clj-http "0.3.6"]]
                  ;; [antler/sandbar "0.4.0-SNAPSHOT"]]
   :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"]
-  :main caribou.api.core
   :ring {:handler caribou.api.core/app
          :servlet-name "caribou-api"
          :init caribou.api.core/init
