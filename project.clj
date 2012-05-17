03322b898fe73bf69a8260bfe9ea50d6f831826f
eric feigner <eric@weareinstrument.com>
2012-05-17 15:39:09 -0700

    changing ring/core to ring, as we need servlet / util code when building servlets

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [antler/caribou-core "0.5.4"]
                  [compojure "1.0.4"]
-                 [ring/ring-core "1.1.0"
+                 [ring "1.1.0"
                   :exclusions [org.clojure/clojure
                                clj-stacktrace
                                hiccup]]
