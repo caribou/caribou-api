18a7ecdccfaacd453ec8d445384d275e2049ef07
Kyle Dawkins <kyle.dawkins@weareinstrument.com>
2013-02-06 17:25:33 -0800

    Version bump to match new core

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject antler/caribou-api "0.8.0"
+(defproject antler/caribou-api "0.8.2"
   :description "The api ring handler for caribou"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [antler/caribou-core "0.8.0"]
+                 [antler/caribou-core "0.8.2"]
                  [compojure "1.1.3" :exclusions [ring/ring-core]]
                  [ring "1.1.6"]
                  [cheshire "4.0.3"]
