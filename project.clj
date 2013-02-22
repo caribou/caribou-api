f114ea618353408e1ef569dbd2c91e5a893211b3
Justin Glenn Smith <noisesmith@gmail.com>
2013-02-22 13:22:43 -0800

    bumping to clojure 1.4.0 for the 0.9.0 version

--- project.clj
+++ project.clj
@@ -1,15 +1,18 @@
 (defproject antler/caribou-api "0.9.0"
   :description "The api ring handler for caribou"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [antler/caribou-core "0.9.0"]
-                 [compojure "1.1.3" :exclusions [ring/ring-core]]
+                 [compojure "1.1.3"
+                  :exclusions [ring/ring-core org.clojure/tools.macro]]
                  [ring "1.1.6"]
                  [cheshire "4.0.3"]
                  [org.clojure/data.xml "0.0.6"]
                  [clojure-csv "1.3.2" :exclusions [org.clojure/clojure]]
                  [swank-clojure "1.4.2"]
                  ;; [antler/sandbar "0.4.0"]
-                 [clj-http "0.5.5"]]
+                 [clj-http "0.5.6"
+                  :exclusions [org.apache.httpcomponents/httpclient
+                               org.apache.httpcomponents/httpcore]]]
   :main caribou.api.core
   :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"]
   :autodoc {:name "Caribou API"
