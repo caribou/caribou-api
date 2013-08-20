ebb412f08961673bb1806a4f34113a662e8bbc67
ryan spangler <ryan.spangler@gmail.com>
2013-08-20 11:28:18 -0700

    version

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject antler/caribou-api "0.11.24"
+(defproject antler/caribou-api "0.12.0"
   :description "The api ring handler for caribou"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [antler/caribou-frontend "0.11.30"]
+                 [antler/caribou-frontend "0.12.0"]
                  [clj-http "0.5.6"
                   :exclusions [org.apache.httpcomponents/httpclient
                                org.apache.httpcomponents/httpcore]]]
