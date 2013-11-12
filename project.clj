e56d4121d2a38803e2e0b5e637046fa7ebd24a74
ryan spangler <ryan.spangler@gmail.com>
2013-11-12 11:53:05 -0800

    releasing 0.13.0

--- project.clj
+++ project.clj
@@ -1,17 +1,11 @@
-(defproject caribou/caribou-api "0.12.36"
+(defproject caribou/caribou-api "0.13.0"
   :description "The api ring handler for caribou"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [caribou/caribou-frontend "0.12.36"]
+                 [caribou/caribou-frontend "0.13.0"]
                  [clj-http "0.5.6"
                   :exclusions [org.apache.httpcomponents/httpclient
                                org.apache.httpcomponents/httpcore]]]
   :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"]
-  :autodoc {:name "Caribou API"
-            :page-title "Caribou API - Documentation"
-            :description
-            "<p>This HTTP API is automatically generated if a database managed by
-             <a href=\"http://antler.github.com/caribou-core\">Caribou Core</a>
-             is present."}
   :ring {:handler caribou.api.core/handler
          :servlet-name "caribou-api"
          :init caribou.api.core/init
