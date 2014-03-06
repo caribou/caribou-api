fae505ce9af833085c34ad57b8bd91ede65d6481
ryan spangler <ryan.spangler@gmail.com>
2014-03-05 16:23:25 -0800

    0.14.0 release

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject caribou/caribou-api "0.13.10"
+(defproject caribou/caribou-api "0.14.0"
   :description "The api ring handler for caribou"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [caribou/caribou-frontend "0.13.10"]
+                 [caribou/caribou-frontend "0.14.0"]
                  [clj-http "0.5.6"
                   :exclusions [org.apache.httpcomponents/httpclient
                                org.apache.httpcomponents/httpcore]]]
