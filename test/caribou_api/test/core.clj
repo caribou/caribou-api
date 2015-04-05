2bc0afaad5a3f4bc5476634e871199791b59c143
Frozenlock <frozenlock@gmail.com>
2015-04-05 15:29:32 -0400

    tests to validate the 2 implemented controllers (index and detail)

--- test/caribou_api/test/core.clj
+++ test/caribou_api/test/core.clj
@@ -9,22 +9,19 @@
             [caribou.util :as util]
             [caribou.db :as db]))
 
-(defmacro with-system-models [config & body]
-  `(do
-     (core/with-caribou ~config ;; run everything with the config environment
-       (try (mig/rollback) ;; clear the db, just to be sure
-            (catch Exception _#))
-       (mig/migrate) ;; initialize the system models (model, field, domain, asset...)    
-       ~@body
-       (mig/rollback)))) ;; clean the db
+(defmacro with-system-models
+  "Create the system models, evaluate the body, remove the system
+  models and return the result obtained with the body.
 
-
-(defn invoke-model-test
-  []
-  (let [model (util/query "select * from model where id = 1")
-        invoked (model/invoke-model (first model))]
-    (testing "Model invocation."
-      (is (= "name" (-> invoked :fields :name :row :slug))))))
+  In addition, the body is already wrapped into a with-caribou."
+  [config & body]
+  `(core/with-caribou ~config ;; run everything with the config environment
+     (try (mig/rollback) ;; clear the db, just to be sure
+          (catch Exception _#))
+     (mig/migrate) ;; initialize the system models (model, field, domain, asset...)    
+     (let [result# (do ~@body)]
+       (mig/rollback) ;; clean the db
+       result#))) ;; return the result of 'body'
 
 
 (defn test-init
@@ -35,116 +32,58 @@
 (defn db-fixture
   [f]
   (test-init)
+  ;; create some initial companies
+  (model/create :model {:name "Company"
+                        :fields [{:name "Name" :type "string"}]})
+  (model/create :company {:name "Acme"}) ;; first item created ---> ID = 1
+  (model/create :company {:name "CorpInc"}) ;; second item created ---> ID = 2
   (f)
-  (doseq [slug [:yellow :purple :zap :chartreuse :fuchsia :base :level
-                :void :white :agent :pinkƒpink1]]
-    (when (db/table? slug) (model/destroy :model (model/models slug :id)))
-    (when (db/table? :everywhere)
-      (do
-        (db/do-sql "delete from locale")
-        (model/destroy :model (model/models :nowhere :id))
-        (model/destroy :model (model/models :everywhere :id))))
-    (when-let [passport (model/pick :asset {:where
-                                            {:filename "passport.picture"}})]
-      (model/destroy :asset (:id passport)))))
+  ;; delete the companies
+  (doseq [slug [:company]]
+    (when (db/table? slug) (model/destroy :model (model/models slug :id)))))
+
+
+;;; All tests should be created to work into the `db-fixture`.
+;;; This allows us to make sure we don't leave traces in the DB.
+
+
+
+(defn invoke-model-test
+  []
+  (let [model (util/query "select * from model where id = 1")
+        invoked (model/invoke-model (first model))]
+    (testing "Model invocation."
+      (is (= "name" (-> invoked :fields :name :row :slug))))))
+
+(defn index-test []
+  (testing "Index response"
+    (let [body (:body (caribou.api.controllers.home/index {:params {:model "company.json"}}))]
+      (is (.contains body "\"name\":\"Acme\""))
+      (is (.contains body "\"name\":\"CorpInc\"")))))
+
+(defn detail-test []
+  (testing "Detail response"
+    (let [body (:body (caribou.api.controllers.home/detail
+                       {:params {:model "company.json" :where "id:2"}}))] ;; id 2 is CorpInc
+      (is (not (.contains body "\"name\":\"Acme\""))) ;; should not contain any reference to Acme
+      (is (.contains body "\"name\":\"CorpInc\"")))))
+
+
 
 
 (defn all-model-tests
   []
-  (db-fixture invoke-model-test))
+  (db-fixture invoke-model-test)
+  (db-fixture index-test)
+  (db-fixture detail-test))
 
+
+;; We test in H2, because it doesn't rely on any external DB.
+;; Other DBs, like postgresql, should already be tested in
+;; caribou-core.
 
 (deftest ^:h2
   h2-tests
   (let [config (test/read-config :h2)]
     (with-system-models config
-      (all-model-tests)
-      )))
-
-
-
-;; (deftest content-list-test
-;;   (sql/with-connection (t/read-config :h2)
-;;     (model/invoke-models)
-;;     (is (> (count (ctrl/index "model" {})) 0))))
-
-;; (deftest content-list-test
-;;   (sql/with-connection (t/read-config :h2)
-;;     (model/invoke-models)
-;;     (is (> (count (content-list "model" {})) 0))))
-
-;; (deftest content-item-test
-;;   (sql/with-connection (t/read-config :h2)
-;;     (model/invoke-models)
-;;     (is (> (count (content-item "model" 1)) 0))))
-
-;; (deftest content-field-test
-;;   (sql/with-connection (t/read-config :h2)
-;;     (model/invoke-models)
-;;     (is (= "model" (content-field "model" 1 :slug)))))
-
-;; ;; TODO: test timestamp fields
-;; ;; (deftest render-test
-;; ;;   (sql/with-connection (t/read-config :h2)
-;; ;;     (let [model (render :model (db/fetch) {})]
-;; ;;       (is (not (model nil)))
-;; ;;       (is (= (model :name) "Foo"))
-;; ;;       (is (= (model :description) "bar"))
-;; ;;       (is (= (model :position) 1))
-;; ;;       (is (= (model :nested) false))
-;; ;;       (is (= (model :abstract) false))
-;; ;;       (is (= (model :ancestor_id) 0)))))
-
-;; (deftest render-field-test
-;;   (sql/with-connection (t/read-config :h2)
-;;     (model/invoke-models)
-;;     (is (= "yayay" (render-field "model" {:description "yayay"} "description" {})))))
-
-;; ;; actions ------------------------------------------------
-;; ;; happy-path action smoke-testing
-
-;; ;; GET home
-;; (deftest home-action-test
-;;   (sql/with-connection (t/read-config :h2)
-;;     (model/invoke-models)
-;;     (let [response (json/read-json (home {}))]
-;;       (is (not (response nil))))))
-
-;; ;; GET list-all
-;; (deftest list-all-action-test
-;;   (sql/with-connection (t/read-config :h2)
-;;     (model/invoke-models)
-;;     (let [response (json/read-json (list-all {:slug "model"}))]
-;;       (is (> (count response) 0)))))
-
-;; ;; TODO
-;; ;; POST create-content
-;; (deftest create-content-action-test)
-
-;; ;; GET model-spec
-;; (deftest model-spec-action-test
-;;   (sql/with-connection (t/read-config :h2)
-;;     (model/invoke-models)
-;;     (let [response (json/read-json (model-spec {:slug "model"}))]
-;;       (is (> (count response) 0)))))
-
-;; ;; GET item-detail
-;; (deftest item-detail-action-test
-;;   (sql/with-connection (t/read-config :h2)
-;;     (model/invoke-models)
-;;     (let [response (json/read-json (item-detail {:slug "model" :id 1}))]
-;;       (is (> (count response) 0)))))
-
-;; ;; TODO
-;; ;; PUT update-content
-;; (deftest update-content-action-test)
-
-;; ;; TODO
-;; ;; PUT delete-content
-;; (deftest delete-content-action-test)
-
-;; ;; GET field-detail
-;; ;; (deftest field-detail-action-test
-;; ;;   (sql/with-connection (t/read-config :h2)
-;; ;;     (let [response (json/read-json (field-detail {:slug "model" :id 1 :field "name"}))]
-;; ;;       (is (= "model" response)))))
+      (all-model-tests))))
