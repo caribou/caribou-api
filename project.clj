cff72a70e0e8639a71007ef57926731624da6705
Frozenlock <frozenlock@gmail.com>
2015-07-07 03:08:55 -0400

    Support nested fields

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject caribou/caribou-api "0.14.2"
+(defproject org.clojars.frozenlock/caribou-api "0.14.2"
   :description "The api ring handler for caribou"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [caribou/caribou-frontend "0.14.0"]
